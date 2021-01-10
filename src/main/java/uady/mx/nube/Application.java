9820006cde834cb103ceaee05bfb80a95b96a057
Edwin Fajardo <erofaba@gmail.com>
2021-01-09 18:19:45 -0600

    Rabbitmq

--- src/main/java/uady/mx/nube/Application.java
+++ src/main/java/uady/mx/nube/Application.java
@@ -1,13 +1,73 @@
+// package uady.mx.nube;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class Application {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(Application.class, args);
+// 	}
+
+// }
 package uady.mx.nube;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  @Value("${sample.rabbitmq.exchange}")
+  String topicExchangeName;
+  @Value("${sample.rabbitmq.routingkey}")
+  String routingkey;
+  @Value("${sample.rabbitmq.queue}")
+  String queueName;
+
+  @Bean
+  Queue queue() {
+    return new Queue(queueName, false);
+  }
+
+  @Bean
+  TopicExchange exchange() {
+    return new TopicExchange(topicExchangeName);
+  }
+
+  @Bean
+  Binding binding(Queue queue, TopicExchange exchange) {
+    return BindingBuilder.bind(queue).to(exchange).with(routingkey);
+  }
+
+  @Bean
+  SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+      MessageListenerAdapter listenerAdapter) {
+    SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+    container.setConnectionFactory(connectionFactory);
+    container.setQueueNames(queueName);
+    container.setMessageListener(listenerAdapter);
+    return container;
+  }
+
+  @Bean
+  MessageListenerAdapter listenerAdapter(Receiver receiver) {
+    return new MessageListenerAdapter(receiver, "receiveMessage");
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    SpringApplication.run(Application.class, args);
+    System.out.println("Servicio iniciado correctamente");
+  }
 
 }
